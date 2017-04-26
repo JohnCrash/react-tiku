76ec1041992334872ade0beb086bae401fdc94c1
john <udsn2@sina.com>
2017-04-26 10:49:43 +0800

    1

--- src/tk/tkeditor.js
+++ src/tk/tkeditor.js
@@ -2,33 +2,59 @@ import React, {Component} from 'react';
 import TkToolBar from './tktoolbar';
 import TkViewer from './tkviewer';
 import TkNavDrawer from './tknavdrawer';
+/* 在屏幕下方弹上一个消息 */
+import Snackbar from 'material-ui/Snackbar';
+import 'whatwg-fetch'
 
 class TkEditor extends Component{
 	constructor(){
 		super();
 		this.state = {
-			isNavDrawerOpen:false
+			isNavDrawerOpen:false,
+			errorOpen:false,
+            errorMsg:'',
 		}
 		this.topics = {
 			text:"hello world"
 		}
 	}
-
+	//弹出一个错误条
+	messageBar(str){
+		this.setState({errorOpen:true,errorMsg:str});
+	}
+    componentDidMount(){
+		//加载练习册
+        fetch('/book').then(function(response){
+            return response.text()
+        }).then(function(data){
+		  this.error = 'error msg'
+          this.setState({books:JSON.parse(data)});
+        }.bind(this)).catch(function(e){
+            //加载数目失败
+			if(this.error)
+          		this.messageBar(this.error);
+			else
+				this.messageBar(e.toString());
+        }.bind(this));
+    }
 	toolReset(){
 		console.log("click reset : " + this.topics.text);
 	}
 
 	toolMenu(){
 		//打开边栏
-		this.setState({isNavDrawerOpen:true});
+		this.drawer.toggle();
 	}
 
 	render(){
 		return (
 			<div>
 				<TkToolBar toolReset={this.toolReset.bind(this)} toolMenu={this.toolMenu.bind(this)}/>
-				<TkNavDrawer open={this.state.isNavDrawerOpen}/>
+				<TkNavDrawer ref={(drawer)=>{this.drawer = drawer}} books={this.state.books} messageBar={this.messageBar.bind(this)}/>
 				<TkViewer />
+				<Snackbar open={this.state.errorOpen} 
+                message={this.state.errorMsg} 
+                autoHideDuration={5000} />
 			</div>
 		);
 	}
--- src/tk/tknavdrawer.js
+++ src/tk/tknavdrawer.js
@@ -1,27 +1,109 @@
 import React, {Component} from 'react';
+/* 边栏 */
 import Drawer from 'material-ui/Drawer';
+/* 工具条 */
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-
+import SelectField from 'material-ui/SelectField';
+import {List, ListItem} from 'material-ui/List';
+import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import TkCloseIcon from 'material-ui/svg-icons/navigation/close';
+import Subheader from 'material-ui/Subheader';
+import Toggle from 'material-ui/Toggle';
+/* 引用的图标 */
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import ContentInbox from 'material-ui/svg-icons/content/inbox';
+import ContentDrafts from 'material-ui/svg-icons/content/drafts';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import IconBook from 'material-ui/svg-icons/file/folder';
+import IconUnit from 'material-ui/svg-icons/editor/format-align-justify';
+import 'whatwg-fetch'
 
 class TkNavDrawer extends Component{
     constructor(props){
         super(props);
         this.state = {
-            openDrawer:false
+            openDrawer:false,
+            books:[],
+            selectBookIndex:0,
+            modules:[],
+            units:{},
         };
-        this.actionClose = false;
     }
     toggle(){
-        this.actionClose = true;
-        this.setState({openDrawer:false},()=>{
-            this.actionClose = false;
-        });
+        this.state.openDrawer = !this.state.openDrawer;
+        this.setState({openDrawer:this.state.openDrawer});
+    }
+    //消息对话栏
+    messageBox(str){
+        if(this.props.messageBar){
+            this.props.messageBar(str);
+        }
+    }
+    componentDidMount(){
+        //加载练习册列表
+    }
+    //选择章
+    handleNestedListToggle(item){
+        this.currentModule = item.props.primaryText;
+        if(!this.state.units[item.props.primaryText]){
+            let request = `/module?Book=${this.currentBook}&Module=${item.props.primaryText}`;
+            fetch(request).then(function(response){
+                return response.text();
+            }.bind(this)).then(function(data){
+                this.error = data;
+                let unit = JSON.parse(data);
+                this.state.units[item.primaryText] = (unit.map((item)=>{
+                    return (<ListItem leftIcon={<IconUnit />} primaryText={unit.BookUnit} />);
+                }));
+                this.setState();
+            }.bind(this)).catch(function(e){
+                //加载出错
+                if(this.error){
+                    this.messageBox(this.error);
+                }else{
+                    this.messageBox(e.toString());
+                }
+            }.bind(this));
+        }
+    }
+    //选择练习册
+    handleSelectBookIndex(event, index, value){
+        this.setState({selectBookIndex:value});
+        this.currentBook = this.props.books[value-1].BookName;
+        let request = `/module?Book=${this.props.books[value-1].BookName}`;
+        fetch(request).then(function(response){
+            return response.text();
+        }.bind(this)).then(function(data){
+            //加载章
+            this.error = data;
+            let key = 1;
+            this.setState({modules:JSON.parse(data).map((item)=>{
+                //映射章
+                return (<ListItem key={key++} 
+                leftIcon={<IconBook />} 
+                primaryText={item.Module}
+                onNestedListToggle={this.handleNestedListToggle.bind(this)}
+                primaryTogglesNestedList={true}
+                nestedItems={this.state.units[item.Module]}
+                />);
+            })});
+        }.bind(this)).catch(function(e){
+            //加载出错
+            if(this.error){
+                this.messageBox(this.error);
+            }else{
+                this.messageBox(e.toString());
+            }
+        }.bind(this));
     }
     render(){
-        if(!this.actionClose){
-            this.state.openDrawer = this.props.open;
+        let books
+        if(this.props.books){
+            let i = 1;
+            books = this.props.books.map((item)=>{
+                return <MenuItem value={i++} leftIcon={<IconBook/>} primaryText={item.BookName} />;
+            });
         }
         return (
             <Drawer ref={(drawer)=>{this.drawer = drawer}}
@@ -34,6 +116,14 @@ class TkNavDrawer extends Component{
                     </IconButton>                                                                     
                     </ToolbarGroup>
                 </Toolbar>
+                <SelectField /* 练习册选择 */
+                    value={this.state.selectBookIndex}
+                    onChange={this.handleSelectBookIndex.bind(this)}>
+                    {books}
+                </SelectField>
+                <List /* 章节层次*/>
+                    {this.state.modules}
+                </List>                    
             </Drawer>
         );
     }
